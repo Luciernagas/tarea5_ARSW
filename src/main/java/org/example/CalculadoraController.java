69ed9c9093bc5b6bc50718101f74737af30ad891
Luciernagas <104604359+Luciernagas@users.noreply.github.com>
2023-06-29 21:00:17 -0500

    version final :)

--- src/main/java/org/example/CalculadoraController.java
+++ src/main/java/org/example/CalculadoraController.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class CalculadoraController {
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CalculadoraController {
+    @Autowired
+    ValorAnterior valorAnterior;
+
+    @GetMapping("/add")
+    public String add(@RequestParam(name = "number", required = false, defaultValue = "0") String valor, Model model){
+        model.addAttribute("valor", valor);
+        valorAnterior.setValorAnterior(valorAnterior.getValorAnterior() + Double.parseDouble(valor));
+        model.addAttribute("resultado", valorAnterior.getValorAnterior());
+        return "calculadora";
+    }
+
+    @GetMapping("/mult")
+    public String mult(@RequestParam(name = "number", required = false, defaultValue = "0") String valor, Model model){
+        model.addAttribute("valor", valor);
+        valorAnterior.setValorAnterior(valorAnterior.getValorAnterior() * Double.parseDouble(valor));
+        model.addAttribute("resultado", valorAnterior.getValorAnterior());
+        return "calculadora";
+    }
+
+    @GetMapping("/rest")
+    public String rest(@RequestParam(name = "number", required = false, defaultValue = "0") String valor, Model model){
+        model.addAttribute("valor", valor);
+        valorAnterior.setValorAnterior(valorAnterior.getValorAnterior() - Double.parseDouble(valor));
+        model.addAttribute("resultado", valorAnterior.getValorAnterior());
+        return "calculadora";
+    }
+
+    @GetMapping("/div")
+    public String div(@RequestParam(name = "number", required = false, defaultValue = "0") String valor, Model model){
+        model.addAttribute("valor", valor);
+        valorAnterior.setValorAnterior(valorAnterior.getValorAnterior() / Double.parseDouble(valor));
+        model.addAttribute("resultado", valorAnterior.getValorAnterior());
+        return "calculadora";
+    }
+
+    @GetMapping("/ac")
+    public String ac(Model model){
+        valorAnterior.setValorAnterior(0);
+        model.addAttribute("valor", valorAnterior.getValorAnterior());
+        return "calculadora";
+    }
+
+    @GetMapping("/calc")
+    public String defaultCalc(){
+        System.out.println("defaultcalc");
+        return "calculadora";
+    }
+
 }
--- src/main/java/org/example/ValorAnterior.java
+++ src/main/java/org/example/ValorAnterior.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ValorAnterior {
+package org.example;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class ValorAnterior {
+    private double valoranterior = 0;
+    public ValorAnterior(){
+    }
+
+    public double getValorAnterior(){
+        return valoranterior;
+    }
+
+    public void setValorAnterior(double newvalue){
+        valoranterior = newvalue;
+    }
 }
